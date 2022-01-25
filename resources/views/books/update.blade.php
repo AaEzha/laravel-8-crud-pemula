a8fa6085de8a8078386581d17692bb21408b8444
AaEzha <aaezha@gmail.com>
2022-01-25 14:20:02 +0700

    08. Error validation

--- resources/views/books/update.blade.php
+++ resources/views/books/update.blade.php
@@ -9,27 +9,35 @@
             <label for="judul">Judul Buku</label>
             <input type="text" class="form-control" name="judul" id="judul" aria-describedby="helpjudul"
                 placeholder="Tulis judul buku" value="{{ $book->judul }}">
-            <small id="helpjudul" class="form-text text-danger">Help text</small>
+            @error('judul')
+            <small id="helpjudul" class="form-text text-danger">{{ $message }}</small>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="penulis">Nama Penulis</label>
             <input type="text" class="form-control" name="penulis" id="penulis" aria-describedby="helppenulis"
                 placeholder="Tulis nama penulis"  value="{{ $book->penulis }}">
-            <small id="helppenulis" class="form-text text-danger">Help text</small>
+            @error('penulis')
+            <small id="helppenulis" class="form-text text-danger">{{ $message }}</small>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="tahun">Tahun Terbit</label>
             <input type="year" class="form-control" name="tahun" id="tahun" aria-describedby="helptahun"
                 placeholder="Tulis tahun terbit"  value="{{ $book->tahun }}">
-            <small id="helptahun" class="form-text text-danger">Help text</small>
+            @error('tahun')
+            <small id="helptahun" class="form-text text-danger">{{ $message }}</small>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="sinopsis">Sinopsis</label>
             <textarea class="form-control" name="sinopsis" id="sinopsis" aria-describedby="helpsinopsis" rows="3" placeholder="Tulis sinopsis">{{ $book->sinopsis }}</textarea>
-            <small id="helpsinopsis" class="form-text text-danger">Help text</small>
+            @error('sinopsis')
+            <small id="helpsinopsis" class="form-text text-danger">{{ $message }}</small>
+            @enderror
         </div>
 
         <input name="simpan" id="simpan" class="btn btn-primary" type="submit" value="Simpan">
